f9af2b4de0c5f403ef6672988745f07b9726c193
inge <inge.anundskas@geodata.no>
2022-03-02 21:13:24 +0100

    v3

--- LabelLayer/Widget.js
+++ LabelLayer/Widget.js
@@ -27,9 +27,12 @@ define([
   'esri/layers/LabelClass',
   'esri/Color',
   //'./widgets/LabelLayer/libs/colorize-main/colorize.js',
-  './widgets/CustomWidgets/arcgis-wab-labellayer/LabelLayer/libs/colorize-main/colorize.js'
+  './widgets/CustomWidgets/arcgis-wab-labellayer/LabelLayer/libs/colorize-main/colorize.js',
+  //'./widgets/LabelLayer/libs/widget-notifications/notifications.js',
+  './widgets/CustomWidgets/arcgis-wab-labellayer/LabelLayer/libs/widget-notifications/notifications.js'
+
 ],  
-function(declare, lang, BaseWidget, utils, LayerStructure, LayerNode, FeatureLayer, FeatureSet, TextSymbol, Font, LabelClass, Color, Gn8Colorize) {
+function(declare, lang, BaseWidget, utils, LayerStructure, LayerNode, FeatureLayer, FeatureSet, TextSymbol, Font, LabelClass, Color, Gn8Colorize, WidgetNotifications) {
   //To create a widget, you need to derive from BaseWidget.
   return declare([BaseWidget], {
     // DemoWidget code goes here
@@ -39,81 +42,104 @@ function(declare, lang, BaseWidget, utils, LayerStructure, LayerNode, FeatureLay
 
     baseClass: 'jimu-widget-labellayer',
     layerStructure: LayerStructure.getInstance(),
+    pointLabelPlacement: ["above-center", "above-left", "above-right", "below-center", "below-left", "below-right", "center-center", "center-left", "center-right"],
+    lineLabelPlacement: ["above-after", "above-along", "above-before", "above-start", "above-end", "below-after", "below-along", "below-before", "below-start", "below-end", "center-after", "center-along", "center-before", "center-start", "center-end"],
+    notification: new WidgetNotifications(),
 
     postCreate: function() {
       this.inherited(arguments);
-      console.log('postCreate');
     },
 
     startup: function() {
       this.inherited(arguments);
-      console.log('startup');
 
       //utils.loadStyleLink('CSSColorPicker', './widgets/LabelLayer/libs/colorize-main/style.css')
+      //utils.loadStyleLink('WidgetNotifications', './widgets/LabelLayer/libs/widget-notifications/style.css')
       utils.loadStyleLink('CSSColorPicker', './widgets/CustomWidgets/arcgis-wab-labellayer/LabelLayer/libs/colorize-main/style.css')
+      utils.loadStyleLink('WidgetNotifications', './widgets/CustomWidgets/arcgis-wab-labellayer/LabelLayer/libs/widget-notifications/style.css')
       
-      let labelBtn = document.querySelector('#label-btn')
-      labelLayerHandler = labelBtn.addEventListener('click', lang.hitch(this,this._labelLayer))
+      document.querySelector('#label-btn')
+      .addEventListener('click', lang.hitch(this,this._labelLayer))
       
-      let layerList = document.querySelector('#label-layer-list')
-      layerListHandler = layerList.addEventListener('change', lang.hitch(this, this._populateFieldsList))
+      document.querySelector('#label-layer-list')
+      .addEventListener('change', lang.hitch(this, this._updateFieldsAndPlacementOptions))
 
-      let colorInput = document.querySelector('#label-color')
-      colorInputHandler = colorInput.addEventListener('focus', lang.hitch(this, this._initColorPicker))
+      document.querySelector('#label-color-preview')
+      .addEventListener('click', lang.hitch(this, this._initColorPicker))
 
+      document.querySelector('#label-color')
+      .addEventListener('change', lang.hitch(this, this._updateLabelColorPreview))
+
+      document.querySelector('#label-halo-color-preview')
+      .addEventListener('click', lang.hitch(this, this._initHaloColorPicker))
+
+      document.querySelector('#label-halo-color')
+      .addEventListener('change', lang.hitch(this, this._updateHaloColorPreview))
     },
 
     onOpen: function() {
-      console.log('onOpen');
-      lang.hitch(this, this._populateLayerList())
-      let selectEl = document.querySelector('#label-layer-list') // Trigger change event
-      selectEl.dispatchEvent(new Event('change'))
+      let layerIds = this.map.graphicsLayerIds
+      if (layerIds.length > 0) {
+        lang.hitch(this, this._updateLayerOptions())
+        let selectEl = document.querySelector('#label-layer-list') // Trigger change event
+        selectEl.dispatchEvent(new Event('change'))
+      } else {
+        this.notification.notify('Ingen kartlag kan tekstes', 'Legg til et kartlag som kan tekstes', 'info')
+      }
     },
 
     onClose: function() {
-      console.log('onClose');
-      let layerList = document.querySelector('#label-layer-list')
-      layerList.innerHTML = ''
-
-      let fieldsList = document.querySelector('#label-field-list')
-      fieldsList.innerHTML = ''
+  
     },
 
-    _populateLayerList: function() {
-      let layerList = document.querySelector('#label-layer-list')
-
+    _updateLayerOptions: function() {
       let layerIds = this.map.graphicsLayerIds
+      let layers = []
+      
       layerIds.forEach(id => {
         let layer = this.map.getLayer(id)
         if (layer.arcgisProps) {
-          layerList.append(
-            this._createElement(
-              `<option value="${id}">${layer.arcgisProps.title}</option>`
-            )  
-          )
+          layers.push({"value" : id, "caption": layer.arcgisProps.title})
         }
       })
+
+      this._updateSelectOptions('label-layer-list', layers)
     },
 
-    _populateFieldsList: function() {
-      let fieldsList = document.querySelector('#label-field-list')
-      fieldsList.innerHTML = ''
-
+    _updateFieldsAndPlacementOptions: function() {
       let layer = this._getSelectedLayer()
       let fields = layer._fields
-      fields.forEach(field => {
-        fieldsList.append(
-          this._createElement(
-            `<option value="${field.name}">${field.alias}</option>`
-          )
-        )
-      })
 
+      this._updateSelectOptions('label-field-list', fields, 'name', 'alias')
+      this._updatePlacementOptions(layer)
+    },
+
+    _updatePlacementOptions(layer) {
+      let placements = []
       if (layer.geometryType === 'esriGeometryPoint') {
         document.querySelector('#label-placement-container').style.display = 'block'
+        placements = this.pointLabelPlacement
+      } else if (layer.geometryType === 'esriGeometryPolyline') {
+        document.querySelector('#label-placement-container').style.display = 'block'
+        placements = this.lineLabelPlacement
       } else {
         document.querySelector('#label-placement-container').style.display = 'none'
       }
+      placements = placements.map(p => ({"value": p, "caption": p}))
+      this._updateSelectOptions('label-placement-list', placements)
+    },
+
+    _updateSelectOptions(id, options, valueKey = 'value', captionKey = 'caption') {
+      let selectNode = document.querySelector(`#${id}`)
+      selectNode.innerHTML = ''
+
+      options.forEach(option => {
+        selectNode.append(
+          this._createElement(
+            `<option value="${option[valueKey]}">${option[captionKey]}</option>`
+          )
+        )
+      })
     },
 
     _initColorPicker: function() {
@@ -127,11 +153,47 @@ function(declare, lang, BaseWidget, utils, LayerStructure, LayerNode, FeatureLay
       colorizer.init().then( 
         success => {
           document.querySelector('#label-color').value = success.hex
+          document.querySelector('#label-color-preview').style.backgroundColor = success.hex
           console.log( success );
         }, error => {
           console.log( error );
         } 
       )
+    },
+
+    _initHaloColorPicker: function() {
+      
+      let data = {
+        "id" : null,
+        "container" : document.querySelector('#colorizer'),
+        "value" : document.querySelector('#label-halo-color').value
+      }
+      let colorizer = new Gn8Colorize(data);
+      colorizer.init().then( 
+        success => {
+          document.querySelector('#label-halo-color').value = success.hex
+          document.querySelector('#label-halo-color-preview').style.backgroundColor = success.hex
+          console.log( success );
+        }, error => {
+          console.log( error );
+        } 
+      )
+    },
+
+    _updateLabelColorPreview: function() {
+      this._updateColorPreview('label-color')
+    },
+
+    _updateHaloColorPreview: function() {
+      this._updateColorPreview('label-halo-color')
+    },
+
+    _updateColorPreview: function(id) {
+      let textColor = document.querySelector(`#${id}`).value
+      let reg=/^#([0-9a-f]{3}){1,2}$/i; // Test for hexadecimal color
+      if (reg.test(textColor)) {
+        document.querySelector(`#${id}-preview`).style.backgroundColor = textColor
+      }
     },
 
     _getSelectedLayer: function() {
@@ -151,9 +213,14 @@ function(declare, lang, BaseWidget, utils, LayerStructure, LayerNode, FeatureLay
       let layerNode = this.layerStructure.getNodeById(layer.id)
 
       if (!layer instanceof FeatureLayer) {
-        this.map.removeLayer(layer)
-        layer = this._convertToFeatureLayer(layer)
-        this.map.addLayer(layer)
+        try {
+          let featureLayer = this._convertToFeatureLayer(layer)
+          this.map.removeLayer(layer)
+          this.map.addLayer(layer)
+          layer = featureLayer
+        } catch(e) {
+          this.notification.notify('Teksting feilet', 'Klarte ikke å konvertere grafikklag til kartobjektlag (FeatureLayer)')
+        }
       }
       
       let labelClass = this._createLabelClass()
@@ -162,12 +229,15 @@ function(declare, lang, BaseWidget, utils, LayerStructure, LayerNode, FeatureLay
     },
 
     _createLabelClass: function() {
-      let labelPlacement = document.querySelector('#label-placement').value ?? 'above-right'
+      let labelPlacement = document.querySelector('#label-placement-list').value ?? 'above-right'
       let textFont = document.querySelector('#label-font-list').value
       let textSize = document.querySelector('#label-font-size').value
       let textColor = document.querySelector('#label-color').value
       let textWeight = document.querySelector('input[name="label-font-weight"]:checked').value ?? 'WEIGHT_NORMAL'
       let labelField = document.querySelector('#label-field-list').value
+      let labelHalo = document.querySelector('#label-halo-checkbox').checked
+      let labelHaloColor = document.querySelector('#label-halo-color').value
+      let labelHaloSize = document.querySelector('#label-halo-size').value
       
       let reg=/^#([0-9a-f]{3}){1,2}$/i; // Test for hexadecimal color
       textColor = (reg.test(textColor)) ? textColor : '#666'
@@ -183,8 +253,13 @@ function(declare, lang, BaseWidget, utils, LayerStructure, LayerNode, FeatureLay
       let labelSymbol = new TextSymbol().setColor(labelColor)
       labelSymbol.font.setSize(`${textSize}pt`)
       labelSymbol.font.setFamily(textFont.toLowerCase())
-      labelSymbol.font.setWeight(fontWeights[textWeight]) 
-
+      labelSymbol.font.setWeight(fontWeights[textWeight])
+      
+      if (labelHalo) {
+        labelSymbol.setHaloColor(new Color(labelHaloColor))
+        labelSymbol.setHaloSize(labelHaloSize)
+      }
+      
       let json = {
         "labelExpressionInfo": {"value": `{${labelField}}`},
         "labelPlacement": labelPlacement
--- LabelLayer/help.md
+++ LabelLayer/help.md
@@ -1,5 +1,8 @@
 # LabelLayers - widget til ArcGIS Web AppBuilder
-LabelLayers er en widget utviklet av Geodata AS for å tekste kartlag direkte i ArcGIS Web AppBuilder. Dette er spesielt nyttig for ad hoc kartlag som legges til i Web AppBuilder og som dermed ikke er satt opp med teksting i det underliggende webkartet som Web AppBuilder bruker. 
+LabelLayers er en widget for å merke kartobjekter med etiketter (labels) direkte i ArcGIS Web AppBuilder. Dette er spesielt nyttig for ad hoc kartlag som legges til i Web AppBuilder og som dermed ikke er satt opp med etiketter i det underliggende webkartet som Web AppBuilder bruker. 
 
 #### Kort om bruken
-Denne widgeten kan brukes til å konfigurere teksting av kartobjekter, både for eksisterende kartlag og kartlag som legges til under bruk (ved hjelp av widgeten "Legg til kartlag"). Eventuelle innstillinger for teksting som gjøres for eksisterende kartlag vil kun gjelde så lenge applikasjonen er i bruk. Neste gang app'en starter vil den benytte eventuelle innstillinger for teksting som er lagret i webkartet som benyttes.  
+Denne widgeten kan brukes til å konfigurere etiketter for kartobjekter, både for eksisterende kartlag og kartlag som legges til under bruk (ved hjelp av widgeten "Legg til kartlag"). Eventuelle innstillinger for etiketter som gjøres for eksisterende kartlag vil kun gjelde så lenge applikasjonen er i bruk. Neste gang app'en starter vil den benytte eventuelle innstillinger for etiketter som er lagret i webkartet.
+
+#### Ved feil eller problemer
+Denne widgeten er basert på ArcGIS og åpen kildekode og ikke underlagt noe brukerstøtte/supportavtale. Eventuelle feil, mangler eller ønsker om forbedringer kan registreres [her](https://github.com/ingean/arcgis-wab-labellayer/issues)
